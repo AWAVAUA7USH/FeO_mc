ded555e30d065897ce11c7825b6a27cccdc72d6f
AWAVAUATUSH <108632636+AWAVAUA7USH@users.noreply.github.com>
2025-11-04 09:56:49 +0100

    Updated Mc_string.rs
    fixed it to use the new method standard for all types

--- src/networking/protocol/data_types/mc_string.rs
+++ src/networking/protocol/data_types/mc_string.rs
@@ -7,26 +7,41 @@ pub struct McString{
     len: i32,
     data: String,
 }
-impl McString{
-    pub fn from_bytes(data: &[u8]) -> Result<Self, ParserError> {
-        let (len, varint_len) = match VarInt::from_bytes(&data[0..5]) {
+impl Construct<&str> for McString {
+    fn new(data: &str) -> Self {
+        let length = data.len() as i32;
+        Self{
+            len: length,
+            data: String::from(data),
+        }
+    }
+}
+impl GetValue for McString {
+    type Output = String;
+    fn value(&self) -> String {
+        self.0
+    }
+}
+impl ToBytes<&str> for McString{
+    fn to_bytes(data: &str) -> Vec<u8>{
+    	let mut output = VarInt::i32_to_bytes(data.len() as i32);
+        output.extend_from_slice(data.as_bytes());
+        output
+    }
+}
+impl FromBytes<String> for McString{
+    fn from_bytes(data: &[u8]) -> Result<(String, usize), ParserError>{
+        let(len, varint_len) = match <VarInt as FromBytes<VarInt>>::from_bytes(&data[0..5]) {
             Ok(value) => value,
             Err(e) => return Err(e),
         };
-        let str_data = data[varint_len..].to_vec();
-        Ok(
-            Self{
-                len: len.0,
-                data: match String::from_utf8(str_data) {
-                    Ok(value) => value,
-                    Err(e) => return Err(ParserError::StringParsingFailed),
-                }
-            }
-        )
-    }
-    pub fn string_to_bytes(data: &str) -> Vec<u8> {
-        let mut output = VarInt::i32_to_bytes(data.len() as i32);
-        output.extend_from_slice(data.as_bytes());
-        output
+        let str_data = data[varint_len..(varint_len+len)].to_vec();
+        Ok((
+            match String::from_utf8(str_data) {
+                Ok(value) => value,
+                Err(e) => return Err(e),
+            },
+            (len as uszie + varint_len),
+        ))
     }
 }
